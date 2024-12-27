c244f0d1dc6f9bd01ead0e70d5782fd7b53071ef
Coderdhanush2003 <dhanush2003.shankar@gmail.com>
2024-12-27 17:32:01 +0530

    changes made

--- Ecommerce/src/components/Products.jsx
+++ Ecommerce/src/components/Products.jsx
@@ -14,6 +14,7 @@ const Products = () => {
     setCartItems,
     Favourites,setFavourites
     ,uniqueId,
+    userLogged
   } = useContext(UserContext);
 
   const [temporaryQuantities, setTemporaryQuantities] = useState({});
@@ -126,7 +127,7 @@ const Products = () => {
   useEffect(() => {
     getCartItems();
     getFavourites();
-  }, []); // Only rerun when uniqueId changes
+  }, [userLogged]); // Only rerun when uniqueId changes
   
   useEffect(() => {
     postCartItems();
